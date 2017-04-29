c33cdc6a3031084f2ae73d4312659f347744a41c
omarkusan <omarkusan@gmail.com>
2017-04-29 11:45:30 -0500

    updating components and adding models

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test',
+    'http://www.vivrebelle.com/assets/site/img/recipe-icon.png')
+  ];
   constructor() { }
 
   ngOnInit() {
